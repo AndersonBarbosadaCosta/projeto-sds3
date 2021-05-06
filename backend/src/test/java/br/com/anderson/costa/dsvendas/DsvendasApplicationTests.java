458824999e9986dfdeedca9a0b2ea303bdbf61f6
Anderson Barbosa da Costa <andersonb.costa2@gmail.com>
2021-05-06 00:22:23 -0300

    Project created

---
+++ backend/src/test/java/br/com/anderson/costa/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.anderson.costa.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
