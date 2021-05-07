cb522291062bacdc5f644362c47e2ee0c4402241
Anderson Barbosa da Costa <andersonb.costa2@gmail.com>
2021-05-06 23:10:45 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
+import DataTable from "./components/DataTable";
+import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <h1 className="text-primary">TESTE REACT</h1>
-      </header>
+   <>
+    <NavBar />
+    <div className="container">
+      <h1 className="text-primary">TESTE REACT</h1> 
+      <DataTable />
     </div>
+    <Footer />
+    </>
   );
 }
 
