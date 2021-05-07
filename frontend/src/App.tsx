cf6df49954236da311f018b9f43293f21517d8db
Anderson Barbosa da Costa <andersonb.costa2@gmail.com>
2021-05-06 21:47:30 -0300

    bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1>TESTE REACT</h1>
+      <h1 className="text-primary">TESTE REACT</h1>
       </header>
     </div>
   );
