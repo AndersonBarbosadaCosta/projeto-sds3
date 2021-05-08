f9871323015f59cb05f3a8a2a6b79275d74880c2
Anderson Barbosa da Costa <andersonb.costa2@gmail.com>
2021-05-08 17:24:54 -0300

    Home, navigation

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,16 +1,18 @@
 
-import imgSvg  from  "./../../assets/img/ds-dark.svg";
+import { Link } from "react-router-dom";
+import imgSvg from "./../../assets/img/ds-dark.svg";
 const NavBar = () => {
-    return (
-        <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
-        <div className="container">
-          <nav className="my-2 my-md-0 mr-md-3">
-          <img src={imgSvg} alt="DevSuperior" width="120" />
-          </nav>
-        </div>
-      </div> 
-    );
-  }
-  
-  export default NavBar;
-  
+  return (
+    <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
+      <div className="container">
+        <nav className="my-2 my-md-0 mr-md-3">
+          <Link to="/">
+            <img src={imgSvg} alt="DevSuperior" width="120" />
+          </Link>
+        </nav>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
